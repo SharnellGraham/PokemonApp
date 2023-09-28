cf04c28c4e7a991374855e4ba0f3fce592e3ad66
Sharnell Graham <sharnellggraham@gmail.com>
2023-09-28 13:15:42 -0400

    part1 api integration in app component

--- pokemon_app/src/App.js
+++ pokemon_app/src/App.js
@@ -1,2 +1,13 @@
 import React,{ useEffect, useState } from "react";
 import PokemonThumbnail from "./Components/PokemonThumbnail";
+
+
+
+
+function App() {
+  const [allPokemons,setAllPokemons] = useState([]);
+  const [loadPoke,setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const getAllPokemons = async () =>{
+    const res = await fetch(loadPoke)
+    const data = await res.json()
+    setLoadPoke(data.next)
