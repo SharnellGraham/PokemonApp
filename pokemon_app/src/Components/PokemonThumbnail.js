4c601a4f22c5ea366c5175652b64454cab15291d
Sharnell Graham <sharnellggraham@gmail.com>
2023-09-28 13:01:59 -0400

    pokemon Detail

--- pokemon_app/src/Components/PokemonThumbnail.js
+++ pokemon_app/src/Components/PokemonThumbnail.js
@@ -10,3 +10,25 @@ const PokemonThumbnail = ({id,name,image,type,height,weight,stat1,stat2,stat3,st
             <div className="number">
                 <small>#0{id}</small>
             </div>
+            
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h3>{name.toUpperCase() }</h3>
+                <small>Type : {type}</small>
+                <button className="pokeinfo" onClick={()=>setShow(!show)}>{show===true?"Know less...":"Know more..."}</button>
+                {show===true?<Description weightpok={weight} heightpok={height} pokstat1={stat1}
+                pokstat2={stat2}
+                pokstat3={stat3}
+                pokstat4={stat4}
+                pokstat5={stat5}
+                pokstat6={stat6}
+                
+                posbs1={bs1}
+                posbs2={bs2}
+                posbs3={bs3}
+                posbs4={bs4}
+                posbs5={bs5}
+                posbs6={bs6}
+                 /> :<></>}
+                
+            </div>
